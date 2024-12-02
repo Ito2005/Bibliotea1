e43157c3cc947e6f29299960a2ab5fd2e3861370
Ito2005 <itoscharmach@gmail.com>
2024-12-02 19:46:10 -0300

    Inclusão do total da compra na listagem de compras

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -9,6 +9,7 @@ class ItensCompraSerializer(ModelSerializer):
         depth = 1
 
 class CompraSerializer(ModelSerializer):
+    fields = ("id", "usuario", "status", "total", "itens")
     itens = ItensCompraSerializer(many=True, read_only=True)
     usuario = CharField(source="usuario.email", read_only=True) # inclua essa linha
     status = CharField(source="get_status_display", read_only=True) # inclua essa linha
