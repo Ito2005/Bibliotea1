e7f9e2173a103481ab5fcb1b81b344e7475f954d
João Vitor <itoscharmach@gmail.com>
2024-12-02 09:26:12 -0300

    Criação dos itens da compra

--- core/admin.py
+++ core/admin.py
@@ -9,6 +9,7 @@ from django.utils.translation import gettext_lazy as _
 
 from core import models
 from core.models import Compra
+from core.models import ItensCompra
 
 
 
@@ -87,5 +88,19 @@ class LivroAdmin(admin.ModelAdmin):
     list_filter = ('editora', 'categoria')
     ordering = ('titulo', 'editora', 'categoria')
     list_per_page = 25
+
+class ItensCompraInline(admin.TabularInline):
+    model = ItensCompra
+    extra = 1 # Quantidade de itens adicionais
+
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    list_display = ("usuario", "status")
+    search_fields = ("usuario", "status")
+    list_filter = ("usuario", "status")
+    ordering = ("usuario", "status")
+    list_per_page = 25
+    inlines = [ItensCompraInline]
 
 admin.site.register(Compra)
