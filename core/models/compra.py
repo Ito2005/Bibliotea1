c7657ec04d04d0b6c29d5abb4ec7d797f5664e19
João Vitor <itoscharmach@gmail.com>
2024-12-16 08:31:06 -0300

    Adicionando tipo de pagamento à entidade de Compra

--- core/models/compra.py
+++ core/models/compra.py
@@ -13,19 +13,34 @@ class Compra(models.Model):
         PAGO = 3, "Pago"
         ENTREGUE = 4, "Entregue"
 
+    class TipoPagamento(models.IntegerChoices):
+        CARTAO_CREDITO = 1, "Cartão de Crédito"
+        CARTAO_DEBITO = 2, "Cartão de Débito"
+        PIX = 3, "PIX"
+        BOLETO = 4, "Boleto"
+        TRANSFERENCIA_BANCARIA = 5, "Transferência Bancária"
+        DINHEIRO = 6, "Dinheiro"
+        OUTRO = 7, "Outro"
+
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="compras")
     status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARRINHO)
     data = models.DateTimeField(auto_now_add=True) # campo novo
+    tipo_pagamento = models.IntegerField(choices=TipoPagamento.choices, default=TipoPagamento.CARTAO_CREDITO)
 
     @property
     def total(self):
         return sum(item.preco * item.quantidade for item in self.itens.all())
 
 class CompraSerializer(ModelSerializer):
+    usuario = CharField(source="usuario.email", read_only=True)
+    status = CharField(source="get_status_display", read_only=True)
+    data = DateTimeField(read_only=True)
+    tipo_pagamento = CharField(source="get_tipo_pagamento_display", read_only=True) # novo campo
+    itens = ItensCompraSerializer(many=True, read_only=True)
+
     class Meta:
         model = Compra
-        fields = "__all__"
-
+        fields = ("id", "usuario", "status", "total", "data", "tipo_pagamento", "itens") # modificado
 
 class ItensCompra(models.Model):
     compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
